fc2e58164c91dd83203e8b317e6817b1436f6e2f
peeedge <pj.harms@gmail.com>
2014-09-11 22:46:39 -0400

    Checking in latest changes

--- app/controllers/winereview_controller.rb
+++ app/controllers/winereview_controller.rb
@@ -1,4 +1,48 @@
 class WineReviewController < ApplicationController
-  def index
+  
+  def show
   end
+
+  def edit
+  end
+
+
+  def index
+	@available_at = Time.now
+	@books = Book.all
+  end
+
+  def new
+  	@wine = Wine.new
+  end
+
+  def create
+  	@wine = Wine.new(wine_params)
+  	@wine.save
+  	redirect_to @wine
+  end
+
+  def update
+  	@wine.update(wine_params)
+  	redirect_to @wine
+  end
+
+  def destroy
+  	@wine.destroy
+  	redirect_to wines_url
+  end
+
+  private
+
+  def wine_params
+  	params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+  end
+
+  before_action :set_wine, only: [:show, :edit, :update, :destroy]
+
+  def set_wine
+  	@wine = Wine.find(params[:id])
+  end
+  
+
 end
