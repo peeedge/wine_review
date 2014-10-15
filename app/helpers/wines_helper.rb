43c0c2902c44ae946b6ef8dc56ebf6775110cf57
peeedge <pj.harms@gmail.com>
2014-10-15 19:24:45 -0400

    Adding Twitter Bootstrap

--- app/helpers/wines_helper.rb
+++ app/helpers/wines_helper.rb
@@ -3,7 +3,7 @@ module WinesHelper
     time.strftime("%B %d, %Y at %l:%M %p")
   end
   def format_average_rating(wine)
-  	average = wine.average_rating
+  	average = wine.format_average_rating
   	if average
     	pluralize(number_with_precision(average, precision: 1), 'star')
   	else
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -6,7 +6,7 @@ class Wine < ActiveRecord::Base
 	   numericality: { only_integer: true, greater_than_or_equal_to: 0 },
 	   unless: "year.blank?"
 	has_many :logentries, dependent: :destroy
-	def average_rating
+	def format_average_rating
 	  if logentries.loaded?
 	    logentries.map(&:rating).compact.average
 	  else
